0410333c6c6aac4831110d2a46194ec31ebc581c
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-05-10 20:15:52 +0000

    Unified build (20190510) script/makefile sfcio

--- Conf/Sfcio_gnu_General.sh
+++ Conf/Sfcio_gnu_General.sh
@@ -19,10 +19,12 @@
  export MPIFC=mpigfortran
 
  export DEBUG="-g -O0"
- export CFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
- export FFLAGS="-O3 -fPIC"
+ export CFLAGS="-O3 -fPIC"
+ export FFLAGS="-O3 -fconvert=big-endian -fimplicit-none -fno-second-underscore -frecord-marker=4 -funroll-loops -Wall -fPIC"
+ export FREEFORM="-ffree-form"
+ export FPPCPP="-cpp"
  export CPPFLAGS="-P -traditional-cpp"
- export MPICFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
+ export MPICFLAGS="-O3 -fPIC"
  export MPIFFLAGS="-O3 -fPIC"
  export MODPATH="-J"
  export I4R4=""
@@ -30,9 +32,8 @@
  export I8R8="-fdefault-integer-8 -fdefault-real-8"
 
  export CPPDEFS=""
- export CFLAGSDEFS=""
- export FFLAGSDEFS="-ffree-form -fconvert=big-endian"
- export FFLAGSDEFS="-ffree-form -fconvert=big-endian -fimplicit-none -fno-second-underscore -frecord-marker=4 -funroll-loops -Wall"
+ export CFLAGSDEFS="-DUNDERSCORE -DLINUX"
+ export FFLAGSDEFS=""
 
  export USECC=""
  export USEFC="YES"
--- Conf/Sfcio_intel_Theia.sh
+++ Conf/Sfcio_intel_Theia.sh
@@ -1,6 +1,4 @@
 # *** for Theia (intel) ***
-#module load intel/18.1.163
-#module load dev/sfcio/1.1.0
  WORKDIR=/scratch3/NCEPDEV/nceplibs/Dexin.Zhang
  MODULEPATH=/apps/lmod/lmod/modulefiles/Core
  module use -a $WORKDIR/modulefiles/core_third
@@ -17,19 +15,21 @@
  export MPIFC=mpiifort
 
  export DEBUG="-g -O0"
- export CFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
- export FFLAGS="-O3 -xHOST -traceback -fPIC"
+ export CFLAGS="-O3 -fPIC"
+ export FFLAGS="-O3 -convert big_endian -assume byterecl -fPIC"
+ export FPPCPP="-cpp"
+ export FREEFORM="-free"
  export CPPFLAGS="-P -traditional-cpp"
- export MPICFLAGS="-O3 -DUNDERSCORE -DLINUX -fPIC"
- export MPIFFLAGS="-O3 -xHOST -traceback -fPIC"
+ export MPICFLAGS="-O3 -fPIC"
+ export MPIFFLAGS="-O3 -fPIC"
  export MODPATH="-module "
  export I4R4="-integer-size 32 -real-size 32"
  export I4R8="-integer-size 32 -real-size 64"
  export I8R8="-integer-size 64 -real-size 64"
 
  export CPPDEFS=""
- export CFLAGSDEFS=""
- export FFLAGSDEFS="-free -convert big_endian -assume byterecl"
+ export CFLAGSDEFS="-DUNDERSCORE -DLINUX"
+ export FFLAGSDEFS=""
 
  export USECC=""
  export USEFC="YES"
