1947c3ac9d601684104a94fb762498b12109471d
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:22:12 +0000

    Unified build (20190618) script/makefile sfcio

--- build_sfcio.sh
+++ build_sfcio.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
 
- (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
-
- sys=${1,,}
- [[ $sys == wcoss || $sys == dell || $sys == cray ||\
-    $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
- debg=false
- inst=false
- skip=false
- local=false
- (( $# > 1 )) && {
-   [[ ${2,,} == build ]] && debg=false
-   [[ ${2,,} == debug ]] && debg=true
-   [[ ${2,,} == install ]] && inst=true
-   [[ ${2,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${2,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${2,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
- (( $# > 2 )) && {
-   [[ ${3,,} == build ]] && debg=false
-   [[ ${3,,} == debug ]] && debg=true
-   [[ ${3,,} == install ]] && inst=true
-   [[ ${3,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${3,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${3,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
-
+ source ./Conf/Analyse_args.sh
  source ./Conf/Collect_info.sh
  source ./Conf/Gen_cfunction.sh
  source ./Conf/Reset_version.sh
@@ -87,20 +56,28 @@ set -x
 #     Install libraries and source files 
 #
    $local && {
-              LIB_DIR4=..
-              INCP_DIR=../include
-              [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
-              INCP_DIR4=$INCP_DIR
-              SRC_DIR=
-             } || {
-              LIB_DIR4=$(dirname $SFCIO_LIB4)
-              INCP_DIR4=$(dirname $SFCIO_INC4)
-              SRC_DIR=$SFCIO_SRC
-              [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
-              [ -d $SFCIO_INC4 ] && { rm -rf $SFCIO_INC4; } \
-                                 || { mkdir -p $INCP_DIR4; }
-              [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
-             }
+     instloc=..
+     LIB_DIR4=$instloc
+     INCP_DIR=$instloc/include
+     [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
+     INCP_DIR4=$INCP_DIR
+     SRC_DIR=
+   } || {
+     [[ $instloc == --- ]] && {
+       LIB_DIR4=$(dirname $SFCIO_LIB4)
+       INCP_DIR4=$(dirname $SFCIO_INC4)
+       SRC_DIR=$SFCIO_SRC
+     } || {
+       LIB_DIR4=$instloc
+       INCP_DIR4=$instloc/include
+       SRC_DIR=$instloc/src
+       [[ $instloc == .. ]] && SRC_DIR=
+     }
+     [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
+     [ -d $SFCIO_INC4 ] && { rm -rf $SFCIO_INC4; } \
+                        || { mkdir -p $INCP_DIR4; }
+     [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
+   }
 
    make clean LIB=
    make install LIB=$sfcioLib4 MOD=$sfcioInc4 \
